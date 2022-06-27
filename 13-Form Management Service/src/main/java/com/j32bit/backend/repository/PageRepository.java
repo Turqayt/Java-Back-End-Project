99e3ca33ada885b487d6c999fc765b71bfdbd00d
Turqay <58841404+Turqayt@users.noreply.github.com>
2022-06-27 22:30:52 +0300

    Add files via upload

---
+++ 13-Form Management Service/src/main/java/com/j32bit/backend/repository/PageRepository.java
@@ -0,0 +1,8 @@
+package com.j32bit.backend.repository;
+
+import com.j32bit.backend.entity.PageV;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PageRepository extends JpaRepository <PageV, Integer> {
+
+}
