5364c4cd0740d3bab87354f84a83b95c663fe834
Turqay <58841404+Turqayt@users.noreply.github.com>
2022-06-30 01:04:18 +0300

    Add files via upload

---
+++ 14 - Page Added, Page Updated, Form Added/src/main/java/com/j32bit/backend/service/UserManagementService.java
@@ -0,0 +1,19 @@
+package com.j32bit.backend.service;
+
+import com.j32bit.backend.dto.user.*;
+
+import java.util.List;
+
+public interface UserManagementService {
+    UserDTO createUser(UserCreateDTO userCreateDTO);//Creat_User
+    List<UserDTO> findUsersWithPaginationAndSorting(int offset, int pageSize, String field); //Read_User Pagination and Sorting
+    List<UserDTO> findAll(String filterText);//Read_User Filtering
+    UserDTO updateUser(Integer id, UserUpdateDTO userUpdateDTO); //Update_User
+    void deleteUser(Integer id); //Delete_User
+    List<UserRoleViewDTO> findUserRolesWithPaginationAndSorting(int offset, int pageSize, String field); //Read_User_Role Pagination and Sorting
+    List<UserRoleViewDTO> findAllRole(String filterText);//Read_User_Role Filtering
+    UserRoleDTO userAuthorization(UserRoleAddedDTO userRoleAddedDTO);//Update_User_Role
+    void deleteRole(Integer id);//Delete_User_Role
+
+
+}
---
+++ 14 - Page Added, Page Updated, Form Added/src/main/java/com/j32bit/backend/service/UserService.java
@@ -0,0 +1,21 @@
+package com.j32bit.backend.service;
+
+import com.j32bit.backend.dto.user.UserDTO;
+import com.j32bit.backend.entity.User;
+
+import java.util.ArrayList;
+
+public interface UserService {
+    /**
+     *
+     * @param username
+     * @return the requested user if it is exists.
+     */
+    public User findByUsername(String username);
+
+    /**
+     *
+     * @return all Users
+     */
+    ArrayList<UserDTO> findAll();
+}
